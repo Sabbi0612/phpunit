6510655eb026ce8c6ad2cc4c1beaf82581007bbe
Sabbi0612 <sabbikohli0612@gmail.com>
2018-03-14 11:13:51 +0530

    Refactor v1.0 ReceipTest.php

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -1,17 +1,25 @@
 <?php
 namespace TDD\Test;
-require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR .'autoload.php';
 
 use PHPUnit\Framework\TestCase;
 use TDD\Receipt;
 
 class ReceiptTest extends TestCase {
+	public function setUp() {
+		$this->Receipt = new Receipt();
+	}
+
+	public function tearDown() {
+		unset($this->Receipt);
+	}
 	public function testTotal() {
-		$Receipt = new Receipt();
+		$input = [0,2,5,8];
+		$output = $this->Receipt->total($input);
 		$this->assertEquals(
-            15,
-            $Receipt->total([0,2,5,8]),
-            'When summed up the total should equal to 15'
+			15,
+			$output,
+			'When summing the total should equal 15'
 		);
 	}
 }
